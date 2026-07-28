2da106c9c2dfab790ce3eadd62297b098c38dfd4
athmasb <manuel.scheibenreif@essity.com>
2026-07-28 17:09:02 +0200

    Initial commit

---
+++ src/main/java/com/learnmanager/LearnManagerApplication.java
@@ -0,0 +1,13 @@
+package com.learnmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearnManagerApplication.class, args);
+  }
+
+}
