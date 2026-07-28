20f5155d2d1b6debde174d99badc875c2fd9c97e
athmasb <manuel.scheibenreif@essity.com>
2026-07-28 18:14:57 +0200

    add PlannedStudySession entity

---
+++ src/main/java/com/learnmanager/entity/PlannedStudySession.java
@@ -0,0 +1,47 @@
+package com.learnmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class PlannedStudySession extends BaseEntity {
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(nullable = false)
+  private User user;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(nullable = false)
+  private StudyModule studyModule;
+
+  @Column(nullable = false, length = 150) private String title;
+
+  @Column(length = 2000) private String description;
+
+  @Column(nullable = false) private LocalDateTime startTime;
+
+  @Column(nullable = false) private LocalDateTime endTime;
+
+  public PlannedStudySession(
+      User user,
+      StudyModule studyModule,
+      String title,
+      String description,
+      LocalDateTime startTime,
+      LocalDateTime endTime) {
+    this.user = user;
+    this.studyModule = studyModule;
+    this.title = title;
+    this.description = description;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
+}
